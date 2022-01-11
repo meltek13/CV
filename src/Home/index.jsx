9160d9cbead40d109f04a89ad10bbe57a8e1e647
meltek13 <regnault.melvin@hotmail.fr>
2022-01-11 14:27:31 +0100

    First commit: Cv texte

---
+++ src/Home/index.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+  <div >
+    <h1>MELVIN REGNAULT</h1>
+    <p>Web Developer</p>
+    <p>Paris</p>
+    <p></p>
+    <h2>POUR VOUS</h2>
+    <p>«Après avoir travailé durant plusieurs années dans le milieu de la restauration, j’ai décidé de m’orienter vers un secteur qui me corespondait davantage : le WEB. Durant ma formation de 6 mois j’ai pu acquérir de nombreuses compétences techniques aussi bien en Front-End qu’en Back-End mais également développer des compétences métiers que ce soit sur le fait de travailer en équipe, savoir être réactif, m’auto-former ou encore être rigoureux dans la propreté de mon code.»</p>
+    <p>Telecharger mon cv <a href="">ici</a></p>
+    <a href="">Decouvrir</a>
+
+    <h2>PORTFOLIO</h2>
+    <h3>Art fmr</h3>
+    <p>Art fmr à été conçu entièrement en Ruby.</p>
+    <p>Ce site est hebergé sur HEROKU, le chargement peut durer quelques instants</p>
+    <button>En savoir plus</button>
+
+    <h3>Casba</h3>
+    <p>Casba a été conçu pour la partie back en Ruby et pour la party front en React.</p>
+    <p>Ce site est hebergé sur HEROKU, le chargement peut durer quelques instants</p>
+    <button>En savoir plus</button>
+
+    <h2>MES COMPETENCES</h2>
+    <p>React</p>
+    <p>Ruby</p>
+    <p>Javascript</p>
+    <p>Html</p>
+    <p>Css</p>
+
+    <h2>METHODES DE TRAVAILS ET OUTILS</h2>
+    <h3>TRAVAIL D'EQUIPE</h3>
+    <p>Peer learning</p>
+    <p>Trello (methode Agile)</p>
+    <p>Discord, Slack </p>
+    
+    <h3>Outils</h3>
+    <p >Github</p>
+    <p >Bootstrap</p>
+    <p >AntDesign</p>
+    <p >Rubocop</p>
+    <p >Heroku</p>
+    <p>...</p>
+    
+    <h3>ENVIRONEMENT</h3>
+    <p >Mac OS</p>
+    <p >Macbook Air</p>
+    <p >Visual studio code</p>
+    <p >iTerm</p>
+
+
+    <h2>CONTACT</h2>
+  </div>
+    )
+};
+
+export default Home;
---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from 'Home';
+
+const App = () => (
+  
+    <Home />
+  
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
